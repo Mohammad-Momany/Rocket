a83897689eeb58289f7738b8edd2608826804774
Mohammad-W-Momani <mohammad.w.momani@gmail.com>
2021-01-12 00:12:44 +0200

    test githup link

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,13 +1,12 @@
 import React, { useState,useEffect } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import axios from "axios";
 import "./App.css";
 const App = () => {
   return (
     <Router>
       <div>
         <h1>hi</h1>
-      </div>
+      </div>  
     </Router>
   );
 };
